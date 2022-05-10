0453690d85701d5fdac889d1cb3c81abe253ba54
Alexandr Siryj <theobroma222@gmail.com>
2022-05-10 23:17:58 +0300

    basic markup

--- react-minting-website/src/App.js
+++ react-minting-website/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { nanoid } from 'nanoid';
 import './App.css';
+import { apes, mainBgImage } from './data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* MAIN BANNER */}
+      <div
+        className="main-card-wrapper"
+        style={{ backgroundImage: `url(${mainBgImage})` }}
+      >
+        <div className="main-card__inner-wrapper">
+          <h1 className="header-txt">React Minting Website</h1>
+          <button type="button" className="main-mint-btn">
+            Mint
+          </button>
+        </div>
+      </div>
+
+      {/* APE LIST */}
+      <div className="cards-wrapper">
+        {apes.map((ape, index) => (
+          <div className="cards-item" key={nanoid()}>
+            <div className="img-wrapper">
+              <img src={ape.img} alt={`ape_${index}`} />
+            </div>
+            <div className="btn-wrapper">
+              <button type="button" className="sm-mint-button">
+                Mint
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
